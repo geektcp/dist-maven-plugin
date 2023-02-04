af694c48bb2fe42da1d126ca547b2e7e661cad34
geektcp <geektcp@163.com>
2023-02-05 05:16:48 +0800

    add code for exec command

--- src/main/java/com/geektcp/common/dist/Goal2Mojo.java
+++ src/main/java/com/geektcp/common/dist/Goal2Mojo.java
@@ -1,5 +1,6 @@
 package com.geektcp.common.dist;
 
+import com.geektcp.common.core.system.Sys;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
@@ -8,14 +9,21 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 
 /**
- *
  * @goal goal2
- * */
+ */
 public class Goal2Mojo extends AbstractMojo {
     @Parameter(name = "name", defaultValue = "test")
     private String name;
 
     public void execute() {
         getLog().info("This is thy.");
+        Sys.p(123);
+
+        try {
+            Runtime.getRuntime().exec("notepad.exe");
+        } catch (Exception e) {
+            Sys.p(e.getMessage());
+        }
     }
+
 }
